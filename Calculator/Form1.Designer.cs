992a5297a2faf1c33bf12026b0df18cf1cf966d6
ShuyaKunita <uni202412_06@telework.to>
2025-02-25 13:24:07 +0900

    Initial commit

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberBox1 = new System.Windows.Forms.TextBox();
+            this.numberBox2 = new System.Windows.Forms.TextBox();
+            this.AdditionButton = new System.Windows.Forms.Button();
+            this.SubtractionButton = new System.Windows.Forms.Button();
+            this.DivisionButton = new System.Windows.Forms.Button();
+            this.MultiplicationButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numberBox1
+            // 
+            this.numberBox1.Location = new System.Drawing.Point(77, 103);
+            this.numberBox1.Name = "numberBox1";
+            this.numberBox1.Size = new System.Drawing.Size(100, 22);
+            this.numberBox1.TabIndex = 0;
+            // 
+            // numberBox2
+            // 
+            this.numberBox2.Location = new System.Drawing.Point(240, 103);
+            this.numberBox2.Name = "numberBox2";
+            this.numberBox2.Size = new System.Drawing.Size(100, 22);
+            this.numberBox2.TabIndex = 1;
+            // 
+            // AdditionButton
+            // 
+            this.AdditionButton.Location = new System.Drawing.Point(73, 320);
+            this.AdditionButton.Name = "AdditionButton";
+            this.AdditionButton.Size = new System.Drawing.Size(45, 30);
+            this.AdditionButton.TabIndex = 2;
+            this.AdditionButton.Text = "+";
+            this.AdditionButton.UseVisualStyleBackColor = true;
+            this.AdditionButton.Click += new System.EventHandler(this.AdditionButton_Click);
+            // 
+            // SubtractionButton
+            // 
+            this.SubtractionButton.Location = new System.Drawing.Point(197, 320);
+            this.SubtractionButton.Name = "SubtractionButton";
+            this.SubtractionButton.Size = new System.Drawing.Size(45, 30);
+            this.SubtractionButton.TabIndex = 3;
+            this.SubtractionButton.Text = "-";
+            this.SubtractionButton.UseVisualStyleBackColor = true;
+            this.SubtractionButton.Click += new System.EventHandler(this.SubtractionButton_Click);
+            // 
+            // DivisionButton
+            // 
+            this.DivisionButton.Location = new System.Drawing.Point(315, 320);
+            this.DivisionButton.Name = "DivisionButton";
+            this.DivisionButton.Size = new System.Drawing.Size(45, 30);
+            this.DivisionButton.TabIndex = 4;
+            this.DivisionButton.Text = "÷";
+            this.DivisionButton.UseVisualStyleBackColor = true;
+            this.DivisionButton.Click += new System.EventHandler(this.DivisionButton_Click);
+            // 
+            // MultiplicationButton
+            // 
+            this.MultiplicationButton.Location = new System.Drawing.Point(429, 319);
+            this.MultiplicationButton.Name = "MultiplicationButton";
+            this.MultiplicationButton.Size = new System.Drawing.Size(45, 30);
+            this.MultiplicationButton.TabIndex = 5;
+            this.MultiplicationButton.Text = "×";
+            this.MultiplicationButton.UseVisualStyleBackColor = true;
+            this.MultiplicationButton.Click += new System.EventHandler(this.MultiplicationButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(569, 320);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(79, 29);
+            this.ResetButton.TabIndex = 6;
+            this.ResetButton.Text = "リセット";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(437, 103);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(37, 15);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.MultiplicationButton);
+            this.Controls.Add(this.DivisionButton);
+            this.Controls.Add(this.SubtractionButton);
+            this.Controls.Add(this.AdditionButton);
+            this.Controls.Add(this.numberBox2);
+            this.Controls.Add(this.numberBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numberBox1;
+        private System.Windows.Forms.TextBox numberBox2;
+        private System.Windows.Forms.Button AdditionButton;
+        private System.Windows.Forms.Button SubtractionButton;
+        private System.Windows.Forms.Button DivisionButton;
+        private System.Windows.Forms.Button MultiplicationButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AdditionButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+        }
+
+        private void SubtractionButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DivisionButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MultiplicationButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
